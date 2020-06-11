b191177ead0c33f3f12133813898494117fdec90
Mario Lazzari <mario.lazzari@gmail.com>
2020-06-11 22:27:01 +0200

    Done lesson 15

--- plinko/score.js
+++ plinko/score.js
@@ -1,7 +1,31 @@
+// observations
+const outputs = [];
+const predictionPoint = 300;
+const k = 3;
+
+// Ran every time a balls drops into a bucket
 function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
-  // Ran every time a balls drops into a bucket
+  outputs.push([dropPosition, bounciness, size, bucketLabel]);
+  //console.log(outputs);
 }
 
+function distance(point) {
+  return Math.abs(point - predictionPoint);
+}
+
+// Write code here to analyze stuff
 function runAnalysis() {
-  // Write code here to analyze stuff
+  const bucket = _.chain(outputs)
+    .map((row) => [distance(row[0]), row[3]])
+    .sortBy((row) => row[0])
+    .slice(0, k)
+    .countBy((row) => row[1])
+    .toPairs()
+    .sortBy((row) => row[1])
+    .last()
+    .first()
+    .parseInt()
+    .value();
+
+  console.log("Bucket:", bucket);
 }
